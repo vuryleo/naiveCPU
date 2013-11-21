1780a0c38f8dbb9e2ef85dad5fbf56a7692bbd92
Ray Song <i@maskray.me>
2013-11-21 17:36:35 +0800

    add assembler: bin/assembler -ba

---
+++ naiveCPU/tests/assembler.s
@@ -0,0 +1,34 @@
+bteqz hello
+btnez hello
+beqz r7 hello
+bnez r7 hello
+hello: addiu r1 3f
+b hello
+addiu3 r1 r2 14
+addsp 32
+addu r1 r2 r3
+and r1 r2
+cmp r3 r4
+cmpi r3 34
+int 10
+jalr r2
+jr r5
+jrra
+lw r4 r2 3
+lw_sp r7 3
+mfih r7
+mfpc r7
+move r1 r2
+mtsp r7
+neg r2 r3
+not r2 r3
+nop
+sll r1 r1 7
+sllv r1 r1
+slt r1 r2
+sltu r1 r1
+sltui r1 1
+subu r1 r1 r1
+sw r1 r1 1
+sw_sp r1 1
+xor r1 r2
