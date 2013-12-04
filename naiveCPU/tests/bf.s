8524954cb75a2c27fbe78f623064e01bc7f99411
Ray Song <i@maskray.me>
2013-12-04 22:42:18 +0800

    bf

--- naiveCPU/tests/bf.s
+++ naiveCPU/tests/bf.s
@@ -7,8 +7,10 @@
 	sll r7 r7 0
 
 main_loop:
-	lw r5 r6 0             # r5 = pos
-	lw r4 r7 0             # r4 = cell[pos]
+	lw r5 r6 0                            # r5 = pos
+	lw r4 r7 0                            # r4 = cell[pos]
+	li r2 0                               # row of next character
+	li r3 0                               # column of next character
 	.if r5 == '+'
 		addiu r4 1
 		sw r7 r4 0
@@ -20,10 +22,18 @@ main_loop:
 	.elif r5 == '>'
 		addiu r7 1
 	.elif r5 == '.'
-		# TODO print a character
+		move r1 r4
+		int 3
+		addiu r3 1
+		.if r3 == 64
+			addiu r2 1
+			li r3 0
+		.end
+	.elif r5 == ','
+		# TODO
 	.elif r5 == '['
 		.if r4 == 0
-			xor r3 r3      # number of unmatched parentheses
+			xor r3 r3             # number of unmatched parentheses
 lparen_loop:
 			lw r4 r7 0
 			.if r4 == '['
@@ -48,5 +58,7 @@ rparen_loop:
 			addiu r3 -1
 		.end
 	.else
-		# TODO error
+		b @exit
 	.end
+	b @main_loop
+exit:
